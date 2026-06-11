5b849ef1b64ff2b40acc1a2f33ad28a90f5f74fd
f3tch <f3tch.169@gmail.com>
2026-06-12 03:14:30 +0500

    db initialization, setting up db schema and exporting db

---
+++ src/utilities/db.ts
@@ -0,0 +1,13 @@
+import Database, { type Database as DBType } from "better-sqlite3";
+
+const db: DBType = new Database("wakatime-widget.sqlite");
+
+db.exec(`
+    CREATE TABLE IF NOT EXISTS users (
+        discord_id TEXT PRIMARY KEY,
+        wakatime_token TEXT NOT NULL,
+        created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+    );
+`);
+
+export { db };
