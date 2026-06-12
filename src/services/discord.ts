a7b6b3b1f8cc986be40ab4677a4f2e6e10b58e0b
f3tch <f3tch.169@gmail.com>
2026-06-12 05:09:49 +0500

    added contexts in setup command

--- src/services/discord.ts
+++ src/services/discord.ts
@@ -1,8 +1,13 @@
-import { SlashCommandBuilder, type ChatInputCommandInteraction } from "discord.js";
+import { InteractionContextType, SlashCommandBuilder, type ChatInputCommandInteraction } from "discord.js";
 
 const widgetSetup: SlashCommandBuilder = new SlashCommandBuilder()
                 .setName("widget-setup")
-                .setDescription("Connect your WakaTime API key and start using the WakaTime widget!");
+                .setDescription("Connect your WakaTime API key and start using the WakaTime widget!")
+                .setContexts(
+                    InteractionContextType.BotDM,
+                    InteractionContextType.PrivateChannel,
+                    InteractionContextType.Guild
+                );
 
 export default {
     data: widgetSetup,
