40cbb66781ad2568dbe84736f4395881455f220c
f3tch <f3tch.169@gmail.com>
2026-06-12 18:21:25 +0500

    schema column name change

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -5,7 +5,7 @@ const db: DBType = new Database("wakatime-widget.sqlite");
 db.exec(`
     CREATE TABLE IF NOT EXISTS users (
         user_id TEXT PRIMARY KEY,
-        wakatime_token TEXT NOT NULL,
+        wt_key TEXT NOT NULL,
         created_at DATETIME DEFAULT CURRENT_TIMESTAMP
     );
 `);
